a646141013d3658bad0699e49fc3b6d1227dd83e
RamziddinCoder <mrramziddindeveloper@gmail.com>
2024-02-27 22:17:35 +0500

    cover

--- js/app.js
+++ js/app.js
@@ -40,19 +40,29 @@ function showHeaderShrink() {
   }
 }
 
-var owl = $(".owl-carousel");
-owl.owlCarousel({
-  items: 4,
+$(".owl-carousel").owlCarousel({
   loop: true,
   margin: 10,
-  nav: true,
-  autoplay: true,
-  autoplayTimeout: 1000,
-  autoplayHoverPause: true,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+    },
+    600: {
+      items: 3,
+      nav: false,
+    },
+    1000: {
+      items: 5,
+      nav: true,
+      loop: false,
+    },
+  },
 });
-$(".play").on("click", function () {
-  owl.trigger("play.owl.autoplay", [10]);
-});
-$(".stop").on("click", function () {
-  owl.trigger("stop.owl.autoplay");
-});
+// $(".play").on("click", function () {
+//   owl.trigger("play.owl.autoplay", [10]);
+// });
+// $(".stop").on("click", function () {
+//   owl.trigger("stop.owl.autoplay");
+// });
